b14666ac1e05bb37b53cd32c5f9581af661137b3
ZharikovaES <z.kate2000@gmail.com>
2022-10-15 21:38:17 +0300

    0.1: created main html, css, js-code

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function main() {
+    const route = document.getElementById("route");
+    const time = document.getElementById("time");
+    const num = document.getElementById("num");
+    const btnResult = document.getElementById("btn-result");
+    const output = document.getElementById("result");
+
+    if (route && time && num && btnResult && output) {
+    }
+}
+
+main();
